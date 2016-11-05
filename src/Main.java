4822f7b097c0081e033cd7014b738aa370492f8c
DragynSlayr <inderpreet_dhillon@hotmail.com>
2016-11-05 15:59:09 -0600

    Half finished

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,13 @@ public class Main {
 	private static LinkedList<Character> variables;
 	private static HashMap<String, String> expressions;
 	private static String[][] truthTable;
+	private static final String IDENTIFIER = "EX";
 
 	public static void main(String[] args) {
 		getInput();
 		parse();
 		evaluate();
+		System.out.println("A");
 
 		printVariables();
 		printExpressions();
@@ -28,11 +30,11 @@ public class Main {
 
 	private static void parse() {
 		variables = new LinkedList<Character>();
-		short index = 0;
+		int index = 0;
 
 		while (index < input.length()) {
 			char c = input.charAt(index);
-			if (c > 64 && c < 91) {
+			if (Character.isLetter(c)) {
 				if (!inList(variables, c)) {
 					variables.add(c);
 				}
@@ -41,18 +43,18 @@ public class Main {
 		}
 
 		index = 0;
-		short index2 = 0;
-		LinkedList<Short> indices = new LinkedList<Short>();
+		int index2 = 0;
+		LinkedList<Integer> indices = new LinkedList<Integer>();
 		expressions = new HashMap<String, String>();
 
 		while (index < input.length()) {
 			char c = input.charAt(index);
 			if (c == '(') {
-				indices.addFirst((short) (index + 1));
+				indices.addFirst(index + 1);
 			} else if (c == ')') {
 				String sub = input.substring(indices.peek(), index);
 				if (!inHashMap(expressions, sub)) {
-					expressions.put("EQ0" + index2, sub);
+					expressions.put(IDENTIFIER + index2, sub);
 					indices.pop();
 					index2++;
 				}
@@ -62,29 +64,87 @@ public class Main {
 	}
 
 	private static void evaluate() {
+		int index = 0;
+		int rowHeight = 1;
 
+		while (index < variables.size()) {
+			rowHeight *= 2;
+			index++;
+		}
+
+		truthTable = new String[1 + rowHeight][variables.size() + expressions.size()];
+
+		index = 0;
+		while (index < variables.size()) {
+			truthTable[0][index] = String.valueOf(variables.get(index));
+			int index2 = 1;
+			while (index2 < truthTable.length) {
+				if ((index2 - 1) % rowHeight < rowHeight / 2) {
+					truthTable[index2][index] = "T";
+				} else {
+					truthTable[index2][index] = "F";
+				}
+				index2++;
+			}
+			index++;
+			rowHeight /= 2;
+		}
+
+		index = 0;
+		while (index < expressions.size()) {
+			String key = IDENTIFIER + index;
+			truthTable[0][index + variables.size()] = expressions.get(key);
+			int index2 = 1;
+			while (index2 < truthTable.length) {
+				String expression = expressions.get(key);
+				truthTable[index2][index + variables.size()] = expression;
+				index2++;
+			}
+			index++;
+		}
 	}
 
 	private static void printVariables() {
 		System.out.println("Output:\nSet of independent variables:");
-		while (!variables.isEmpty()) {
-			System.out.println(variables.poll());
+		int index = 0;
+		int size = variables.size();
+		while (index < size) {
+			char next = variables.pop();
+			System.out.println(next);
+			variables.add(next);
+			index++;
 		}
 	}
 
 	private static void printExpressions() {
 		System.out.println("Set of logical subexpressions and logical expression:");
-		for (String k : expressions.keySet()) {
-			System.out.printf("%s, %s\n", k, expressions.get(k));
+		int index = 0;
+		int size = expressions.size();
+		while (index < size) {
+			String key = "EQ" + index;
+			System.out.printf("%s, %s\n", key, expressions.get(key));
+			index++;
 		}
 	}
 
 	private static void printTruthTable() {
+		System.out.println("Truth Table:");
+		int index = 0;
+		while (index < truthTable.length) {
+			int index2 = 0;
+			while (index2 < truthTable[index].length) {
+				System.out.print(truthTable[index][index2] + "\t");
+				index2++;
+			}
+			System.out.println();
+			index++;
+		}
 	}
 
 	private static boolean inList(LinkedList<Character> l, Character o) {
-		short index = 0;
-		while (index < l.size()) {
+		int index = 0;
+		int size = l.size();
+		while (index < size) {
 			if (l.get(index) == o) {
 				return true;
 			}
@@ -94,11 +154,16 @@ public class Main {
 	}
 
 	private static boolean inHashMap(HashMap<String, String> h, String s) {
-		for (String k : h.keySet()) {
-			if (h.get(k).equals(s)) {
+		int index = 0;
+		int size = h.size();
+		while (index < size) {
+			String key = IDENTIFIER + index;
+			if (h.get(key).equals(s)) {
 				return true;
 			}
+			index++;
 		}
 		return false;
 	}
+
 }
