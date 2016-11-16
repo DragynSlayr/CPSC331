f5014236f6b03f261e0c3bf05d76ec9d292cb5de
DragynSlayr <inderpreet_dhillon@hotmail.com>
2016-11-16 12:15:53 -0700

    Replaced whiles with for

--- src/Main.java
+++ src/Main.java
@@ -36,25 +36,23 @@ public class Main {
 		expressions = new HashMap<String, String>();
 		Stack<Integer> indices = new Stack<Integer>();
 
-		int index = 0;
 		int index2 = 0;
 
-		while (index < input.length()) {
-			char c = input.charAt(index);
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
 			if (Character.isLetter(c)) {
 				if (indexOf(variables, c) == -1) {
 					variables.add(c);
 				}
 			} else if (c == '(') {
-				indices.push(index + 1);
+				indices.push(i + 1);
 			} else if (c == ')') {
-				String sub = input.substring(indices.pop(), index);
+				String sub = input.substring(indices.pop(), i);
 				if (indexOf(expressions, sub) == -1) {
 					expressions.put(IDENTIFIER + index2, sub);
 					index2++;
 				}
 			}
-			index++;
 		}
 
 		rowHeight = power(2, variables.size());
@@ -63,25 +61,21 @@ public class Main {
 	private static void evaluate() {
 		truthTable = new String[1 + rowHeight][variables.size() + expressions.size()];
 
-		int index = 0;
-		while (index < truthTable[0].length) {
+		for (int i = 0; i < truthTable[0].length; i++) {
 			String key = "";
 			String expression = "";
-			if (index < variables.size()) {
-				key = String.valueOf(variables.get(index));
+			if (i < variables.size()) {
+				key = String.valueOf(variables.get(i));
 				expression = key;
 			} else {
-				key = IDENTIFIER + (index - variables.size());
+				key = IDENTIFIER + (i - variables.size());
 				expression = expressions.get(key);
 			}
-			truthTable[0][index] = key;
+			truthTable[0][i] = key;
 			String value = getTruthValues(expression);
-			int index2 = 1;
-			while (index2 < truthTable.length) {
-				truthTable[index2][index] = String.valueOf(value.charAt(index2 - 1));
-				index2++;
+			for (int j = 1; j < truthTable.length; j++) {
+				truthTable[j][i] = String.valueOf(value.charAt(j - 1));
 			}
-			index++;
 		}
 	}
 
@@ -95,32 +89,25 @@ public class Main {
 
 	private static void printExpressions() {
 		System.out.println("Set of logical subexpressions and logical expression:");
-		int index = 0;
-		int size = expressions.size();
-		while (index < size) {
-			String key = IDENTIFIER + index;
+		for (int i = 0; i < expressions.size(); i++) {
+			String key = IDENTIFIER + i;
 			System.out.printf("%s, %s\n", key, expressions.get(key));
-			index++;
 		}
 	}
 
 	private static void printTruthTable() {
 		System.out.println("Truth Table:");
-		int index = 0;
-		while (index < truthTable.length) {
-			int index2 = 0;
-			while (index2 < truthTable[index].length) {
-				if (index == 0) {
-					System.out.print(truthTable[index][index2] + "\t");
+		for (int i = 0; i < truthTable.length; i++) {
+			for (int j = 0; j < truthTable[i].length; j++) {
+				if (i == 0) {
+					System.out.print(truthTable[i][j] + "\t");
 				} else {
-					char[] pad = new char[truthTable[0][index2].length() / 2];
+					char[] pad = new char[truthTable[0][j].length() / 2];
 					String padding = new String(pad).replace('\0', ' ');
-					System.out.print(padding + truthTable[index][index2] + "\t");
+					System.out.print(padding + truthTable[i][j] + "\t");
 				}
-				index2++;
 			}
 			System.out.println();
-			index++;
 		}
 	}
 
@@ -164,9 +151,8 @@ public class Main {
 		}
 		Stack<String> stack = new Stack<String>();
 		String prefix = toPrefix(expression);
-		int index = prefix.length() - 1;
-		while (index > -1) {
-			char c = prefix.charAt(index);
+		for (int i = prefix.length() - 1; i > -1; i--) {
+			char c = prefix.charAt(i);
 			if (Character.isLetter(c)) {
 				stack.push(getTruthColumn(c));
 			} else {
@@ -178,7 +164,6 @@ public class Main {
 					stack.push(and(stack.pop(), stack.pop()));
 				}
 			}
-			index--;
 		}
 		return stack.pop();
 	}
@@ -186,10 +171,9 @@ public class Main {
 	private static String getTruthColumn(char c) {
 		String values = "";
 		int split = rowHeight / power(2, indexOf(variables, c) + 1);
-		int index = 0;
 		int counter = 0;
 		boolean isTrue = true;
-		while (index < rowHeight) {
+		for (int i = 0; i < rowHeight; i++) {
 			if (counter == split) {
 				counter = 0;
 				isTrue = !isTrue;
@@ -200,7 +184,6 @@ public class Main {
 				values += 'F';
 			}
 			counter++;
-			index++;
 		}
 		return values;
 	}
